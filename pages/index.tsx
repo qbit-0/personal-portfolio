9c2499090723fceef2c67e88664191d1c125d956
Duy Pham <duypham12241999@gmail.com>
2022-10-14 16:29:52 -0700

    Update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import Head from "next/head";
 import { FC } from "react";
 import About from "../components/About";
 import Competition from "../components/Competition";
@@ -6,7 +7,6 @@ import Contact from "../components/Contact";
 import FixedNavBar from "../components/FixedNavBar";
 import Footer from "../components/Footer";
 import Home from "../components/Home";
-import HomeCanvas from "../components/HomeBackground/HomeCanvas";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import NavProvider from "../utility/context/NavProvider";
@@ -15,21 +15,26 @@ type HomeProps = {};
 
 const Index: FC<HomeProps> = (props) => {
   return (
-    <NavProvider>
-      <Box component="div" minHeight="100vh">
-        {/* <Box component="div" position="absolute" width="100%" height="100%">
+    <>
+      <Head>
+        <title>Duy Pham</title>
+      </Head>
+      <NavProvider>
+        <Box component="div" minHeight="100vh">
+          {/* <Box component="div" position="absolute" width="100%" height="100%">
           <HomeCanvas />
         </Box> */}
-        <FixedNavBar />
-        <Home />
-        <About />
-        <Projects />
-        <Skills />
-        <Competition />
-        <Contact />
-        <Footer />
-      </Box>
-    </NavProvider>
+          <FixedNavBar />
+          <Home />
+          <About />
+          <Projects />
+          <Skills />
+          <Competition />
+          <Contact />
+          <Footer />
+        </Box>
+      </NavProvider>
+    </>
   );
 };
 
