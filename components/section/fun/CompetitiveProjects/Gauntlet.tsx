f7646e83a6a6d365a18e83baf2cd2a466fe5c527
Duy Pham <duypham12241999@gmail.com>
2023-01-04 16:31:16 -0800

    Fix build errors

--- components/section/fun/CompetitiveProjects/Gauntlet.tsx
+++ components/section/fun/CompetitiveProjects/Gauntlet.tsx
@@ -11,7 +11,6 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { fadeInProps } from "../../utility/other/fadeInProps";
 import CustomImageListItem from "./CustomImageListItem";
 import CompetitiveProjectSummary from "./CompetitiveProjectSummary";
 
@@ -22,7 +21,7 @@ const Gauntlet = (props: Props) => {
   const largeLayout = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
-    <Accordion elevation={1} {...fadeInProps}>
+    <Accordion elevation={1}>
       <CompetitiveProjectSummary
         competition={"MESA Virtual Computer Science Competition"}
         submission={"Gauntlet"}
