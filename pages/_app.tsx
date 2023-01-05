cf86eb3df8a778df81c24cf6500e40d5aaf92c27
Duy Pham <duypham12241999@gmail.com>
2023-01-04 17:44:30 -0800

    Remove unused packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import lightTheme from "../styles/theme/lightTheme";
-import { Analytics } from "@vercel/analytics/react";
 
-import "@fontsource/public-sans";
 import "@fontsource/roboto";
-import { enableMapSet } from "immer";
-import { useEffect } from "react";
 
 const MyApp: React.FC<AppProps> = (props) => {
   const { Component, pageProps } = props;
-  useEffect(() => {
-    enableMapSet();
-  }, []);
 
   return (
     <ThemeProvider theme={lightTheme}>
